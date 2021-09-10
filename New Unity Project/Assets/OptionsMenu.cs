8ed010493c60cbcac8a16b8d3278629d882077a3
Luciano Belardo <belardoluciano@gmail.com>
2021-09-10 17:18:44 -0300

    Agregado de detalles al menu de opciones e interacción ida y vuelta con el game

---
+++ New Unity Project/Assets/OptionsMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public Text valueText;
+    public void OnSliderChanged(float value) {
+        valueText.text = value.ToString();
+    }
+}
