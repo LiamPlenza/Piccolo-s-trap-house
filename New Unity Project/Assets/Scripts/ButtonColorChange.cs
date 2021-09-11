0c25737e4d2dc1b1110f8d6285ca33c76a19697d
Tomas-Wardoloff <tomaswardoloff@gmail.com>
2021-09-11 15:54:28 -0300

    organizacion del menu de opciones

---
+++ New Unity Project/Assets/Scripts/ButtonColorChange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonColorChange : MonoBehaviour
+{
+    public Button button;
+    public Color wantedColor;
+
+    public void ChangeButtonColor()
+    {
+
+    }
+}
---
+++ New Unity Project/Assets/Scripts/SliderController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+// para obtener los valores del slider
+
+public class SliderController : MonoBehaviour
+{
+    public Text valueText;
+    public void OnSliderChanged(float value) {
+        valueText.text = value.ToString();
+    }
+}
