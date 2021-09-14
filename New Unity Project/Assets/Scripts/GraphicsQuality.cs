0acd089a1d7d2e5c0d07681c6ef8f90212101500
Tomas-Wardoloff <tomaswardoloff@gmail.com>
2021-09-14 13:07:51 -0300

    Agregado de la musica y funcionalidad al slider del volumen y a los botones de los graficos

---
+++ New Unity Project/Assets/Scripts/GraphicsQuality.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphicsQuality : MonoBehaviour
+{
+    public void SetQuality(int qualityIndex)
+    {
+        Debug.Log(qualityIndex);
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+}
