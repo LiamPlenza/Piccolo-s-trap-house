edb8a896cafb0e47b4dd5d82d6e40d5443c82901
guillermopecorari <guillepeco12345@gmail.com>
2021-10-17 23:58:01 -0300

    agrego scripts para inventario y avance del inventario

---
+++ Piccolo's trap house (unity project)/Assets/Scripts/Slot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    public GameObject item;
+    public int ID;
+    public string type;
+    public string description;
+    public Sprite icon;
+
+    public bool empty;
+}
