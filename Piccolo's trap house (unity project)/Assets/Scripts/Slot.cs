0759ad701a5c0b639d0be12827ff22dc03306917
guillermopecorari <guillepeco12345@gmail.com>
2021-10-19 00:16:30 -0300

    Correcion de inventario y de animacion

--- Piccolo's trap house (unity project)/Assets/Scripts/Slot.cs
+++ Piccolo's trap house (unity project)/Assets/Scripts/Slot.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
-public class Slot : MonoBehaviour, IPointerClickHandler
+
+public class Slot : MonoBehaviour
 {
     public GameObject item;
     public int ID;
@@ -20,19 +20,9 @@ public class Slot : MonoBehaviour, IPointerClickHandler
     {
         slotIconGameObject = transform.GetChild(0);
     }
-
+    
     public void UpdateSlot()
     {
         slotIconGameObject.GetComponent<Image>().sprite = icon;
-    }
-
-    public void UseItem()
-    {
-        item.GetComponent<Item>().ItemUsage();
-    }
-
-    public void OnPointerClick(PointerEventData pointerEventData)
-    {
-        UseItem();
     }
 }
