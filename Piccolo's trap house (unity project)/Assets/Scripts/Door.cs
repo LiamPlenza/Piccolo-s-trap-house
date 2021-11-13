bae2162f2a65fbc2a5c7a99919e33f78448359de
Tomas-Wardoloff <tomaswardoloff@gmail.com>
2021-11-13 17:57:28 -0300

    abrir y cerrar puertas

---
+++ Piccolo's trap house (unity project)/Assets/Scripts/Door.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : Interactable
+{
+    public Animator myDoor = null;
+
+    public bool isOpen = false;
+
+    public bool isClose = true;
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        if (isClose == true)
+            OpenDoor();
+        else if (isOpen)
+            CloseDoor();
+    }
+
+    public void OpenDoor()
+    {
+        Debug.Log("Open door");
+        myDoor.Play("DoorOpen", 0, 0.0f);
+        isClose = false;
+        isOpen = true;
+    }
+
+    public void CloseDoor()
+    {
+        Debug.Log("Close door");
+        myDoor.Play("DoorClose", 0, 0.0f);
+        isClose = true;
+        isOpen = false;
+    }
+}
